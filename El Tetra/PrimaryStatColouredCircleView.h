dee2edc6d73fc5574f70ac4856b911759062bf30
Matthew Kameron <mkameron@sli-5890.fritz.box>
2012-12-24 19:30:55 +0800

    Created the protocol

---
+++ El Tetra/PrimaryStatColouredCircleView.h
@@ -0,0 +1,26 @@
+//
+//  ColouredCircleWithNumberView.h
+//  El Tetra
+//
+//  Created by Matthew Kameron on 24/12/12.
+//  Copyright (c) 2012 Matthew Kameron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class PrimaryStatColouredCircleView;
+
+#define ELEMENT_FIRE @"Fire"
+#define ELEMENT_AIR @"Air"
+#define ELEMENT_WATER @"Water"
+#define ELEMENT_EARTH @"Earth"
+#define ELEMENT_CHI @"Chi"
+
+@protocol PrimaryStatColouredCircleView <NSObject>
+- (NSNumber *)getNumberForCircle:(PrimaryStatColouredCircleView *)source;
+- (NSString *)getElementForCircle:(PrimaryStatColouredCircleView *)source;
+@end
+
+@interface PrimaryStatColouredCircleView : UIView
+@property (nonatomic, weak) id <PrimaryStatColouredCircleView> dataSource;
+@end
---
+++ El Tetra/PrimaryStatColouredCircleView.m
@@ -0,0 +1,31 @@
+//
+//  ColouredCircleWithNumberView.m
+//  El Tetra
+//
+//  Created by Matthew Kameron on 24/12/12.
+//  Copyright (c) 2012 Matthew Kameron. All rights reserved.
+//
+
+#import "PrimaryStatColouredCircleView.h"
+
+@implementation PrimaryStatColouredCircleView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
