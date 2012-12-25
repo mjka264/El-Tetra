c4baeb514e3ab0721cae59e47c57d6e530d90dd4
Matthew Kameron <mkameron@sli-5890.fritz.box>
2012-12-25 13:22:45 +0800

    Lyle just arrived, in the process of corecting the ADT

--- El Tetra/CharacterData.h
+++ El Tetra/CharacterData.h
@@ -8,52 +8,48 @@
 
 #import <Foundation/Foundation.h>
 
-#define CHARACTER_SOUL_BODY @"Body"
-#define CHARACTER_SOUL_MIND @"Mind"
-#define CHARACTER_SOUL_SPIRIT @"Spirit"
+typedef enum {
+    CharacterDataStatGroupSoul = 1,
+    CharacterDataStatGroupPrimary = 2,
+    CharacterDataStatGroupFireSkills = 3,
+    CharacterDataStatGroupAirSkills = 4,
+    CharacterDataStatGroupWaterSkills = 5,
+    CharacterDataStatGroupEarthSkills = 6,
+    CharacterDataStatGroupAbilities = 7
+} t_characterDataStatGroup;
 
-#define CHARACTER_PRIMARY_FEROCITY @"Ferocity"
-#define CHARACTER_PRIMARY_ACCURACY @"Accuracy"
-#define CHARACTER_PRIMARY_AGILITY @"Agility"
-#define CHARACTER_PRIMARY_RESILIENCE @"Resilience"
-#define CHARACTER_PRIMARY_CHI @"Chi"
+typedef enum {
+    CharacterDataElementFire = 1,
+    CharacterDataElementAir = 2,
+    CharacterDataElementWater = 3,
+    CharacterDataElementEarth = 4,
+    CharacterDataElementChi = 5,
+    CharacterDataElementFire = 6,
+    CharacterDataElementAir = 7,
+    CharacterDataElementWater = 8,
+    CharacterDataElementEarth = 9,
+} t_characterDataElement;
 
-#define CHARACTER_SKILL_FIRE_BODY @"Power, speed, lifting"
-#define CHARACTER_SKILL_FIRE_MIND @"Instinct, innovation, insight, synthesis"
-#define CHARACTER_SKILL_FIRE_SPIRIT @"Passion, agression, assertion, intimidation"
-#define CHARACTER_SKILL_AIR_BODY @"Craft, lockpicking"
-#define CHARACTER_SKILL_AIR_MIND @"Logic, science, reasoning, investigation, memory"
-#define CHARACTER_SKILL_AIR_SPIRIT @"Culture, disguise, trickery, cunning"
-#define CHARACTER_SKILL_WATER_BODY @"Balance, stealth, reflexes"
-#define CHARACTER_SKILL_WATER_MIND @"Language, interpretation"
-#define CHARACTER_SKILL_WATER_SPIRIT @"Empathy, charm, misdirection"
-#define CHARACTER_SKILL_EARTH_BODY @"Fortitude, stamina, labour"
-#define CHARACTER_SKILL_EARTH_MIND @"Perception, observation, procedure, tracking"
-#define CHARACTER_SKILL_EARTH_SPIRIT @"Confidence, bravery, diligence, patience"
-
-#define CHARACTER_ABILITY_INITIATIVE @"Initiative"
-#define CHARACTER_ABILITY_ARTFULWEAPONS @"Artful weapons"
-#define CHARACTER_ABILITY_BRUTALWEAPONS @"Brutal weapons"
-#define CHARACTER_ABILITY_PROJECTILEWEAPONS @"Projectile weapons"
-#define CHARACTER_ABILITY_CRITICALSTRIKES @"Critical strikes"
-#define CHARACTER_ABILITY_REND @"Rend"
-#define CHARACTER_ABILITY_DEFENSIVEPAUSE @"Defensive pause"
-#define CHARACTER_ABILITY_STRENGTHWITHIN @"Strength within"
-#define CHARACTER_ABILITY_MULTIPLEATTACKS @"Multiple attacks"
-#define CHARACTER_ABILITY_LEAPFLOAT @"Leap/float"
-#define CHARACTER_ABILITY_SPELLSWORD @"Spellsword"
-#define CHARACTER_ABILITY_DAOISM @"Daoism"
-#define CHARACTER_ABILITY_OFFWORLDCONTACT @"Offworld contact"
-#define CHARACTER_ABILITY_PRIMALFIRE @"Primal fire"
-#define CHARACTER_ABILITY_PRIMALAIR @"Primal air"
-#define CHARACTER_ABILITY_PRIMALWATER @"Primal water"
-#define CHARACTER_ABILITY_PRIMALEARTH @"Primal earth"
-#define CHARACTER_ABILITY_SHIFTINGBODY @"Shifting (body)"
-#define CHARACTER_ABILITY_SHIFTINGMINDSPIRIT @"Shifting (mind, spirit)"
-
-typedef const char * t_characterData;
+//typedef NSString* t_characterData;
 
 @interface CharacterData : NSObject
 - (id)dataWithAllStats;
-- (id)dataWithStatsInGroup:(t_characterData)group;
+- (id)dataWithStatGroup:(t_characterData)group;
++ (NSString *)headingFrom:(id)characterData;
++ (NSInteger)numberOfSectionsFrom:(id)characterData;
++ (NSString *)numberOfEntriesFrom:(id)characterData atSection:(NSInteger)section;
++ (NSString *)sectionHeadingFrom:(id)characterData atSection:(NSInteger)section;
++ (NSString *)itemDescriptionFrom:(id)characterData atIndexPath:(NSIndexPath *)indexPath;
++ (NSNumber *)itemValueFrom:(id)characterData atIndexPath:(NSIndexPath *)indexPath;
++ (t_characterDataElement)itemElementFrom:(id)characterData atIndexPath:(NSIndexPath *)indexPath;
 @end
+
+
+
+
+
+
+
+
+
+
--- El Tetra/CharacterData.m
+++ El Tetra/CharacterData.m
@@ -9,22 +9,84 @@
 #import "CharacterData.h"
 
 @interface CharacterData ()
-@property (nonatomic, strong) NSDictionary *soulStats;
-@property (nonatomic, strong) NSDictionary *primaryStats;
-@property (nonatomic, strong) NSDictionary *abilityStats;
-@property (nonatomic, readonly, strong) NSDictionary *skillStats;
-+ (NSOrderedSet *)soulStatsPresentationOrder;
-+ (NSOrderedSet *)primaryStatsPresentationOrder;
-+ (NSOrderedSet *)fireSkillsPresentationOrder;
-+ (NSOrderedSet *)airSkillsPresentationOrder;
-+ (NSOrderedSet *)waterSkillsPresentationOrder;
-+ (NSOrderedSet *)earthSkillsPresentationOrder;
-+ (NSOrderedSet *)fireAbilitiesPresentationOrder;
-+ (NSOrderedSet *)airAbilitiesPresentationOrder;
-+ (NSOrderedSet *)waterAbilitiesPresentationOrder;
-+ (NSOrderedSet *)earthAbilitiesPresentationOrder;
-+ (NSOrderedSet *)chiAbilitiesPresentationOrder;
+@property (nonatomic, strong) NSArray *soulDescriptions;
+@property (nonatomic, strong) NSArray *primaryStatDescriptions;
+@property (nonatomic, strong) NSArray *fireSkillDescriptions;
+@property (nonatomic, strong) NSArray *airSkillDescriptions;
+@property (nonatomic, strong) NSArray *waterSkillDescriptions;
+@property (nonatomic, strong) NSArray *earthSkillDescriptions;
+@property (nonatomic, strong) NSArray *abilityDescriptions;
 @end
+
+
+
+#define CHARACTER_SOUL_BODY @"Body"
+#define CHARACTER_SOUL_MIND @"Mind"
+#define CHARACTER_SOUL_SPIRIT @"Spirit"
+
+#define CHARACTER_PRIMARY_FEROCITY @"Ferocity"
+#define CHARACTER_PRIMARY_ACCURACY @"Accuracy"
+#define CHARACTER_PRIMARY_AGILITY @"Agility"
+#define CHARACTER_PRIMARY_RESILIENCE @"Resilience"
+#define CHARACTER_PRIMARY_CHI @"Chi"
+
+#define CHARACTER_SKILL_FIRE_BODY @"Power, speed, lifting"
+#define CHARACTER_SKILL_FIRE_MIND @"Instinct, innovation, insight, synthesis"
+#define CHARACTER_SKILL_FIRE_SPIRIT @"Passion, agression, assertion, intimidation"
+#define CHARACTER_SKILL_AIR_BODY @"Craft, lockpicking"
+#define CHARACTER_SKILL_AIR_MIND @"Logic, science, reasoning, investigation, memory"
+#define CHARACTER_SKILL_AIR_SPIRIT @"Culture, disguise, trickery, cunning"
+#define CHARACTER_SKILL_WATER_BODY @"Balance, stealth, reflexes"
+#define CHARACTER_SKILL_WATER_MIND @"Language, interpretation"
+#define CHARACTER_SKILL_WATER_SPIRIT @"Empathy, charm, misdirection"
+#define CHARACTER_SKILL_EARTH_BODY @"Fortitude, stamina, labour"
+#define CHARACTER_SKILL_EARTH_MIND @"Perception, observation, procedure, tracking"
+#define CHARACTER_SKILL_EARTH_SPIRIT @"Confidence, bravery, diligence, patience"
+
+#define CHARACTER_ABILITY_INITIATIVE @"Initiative"
+#define CHARACTER_ABILITY_ARTFULWEAPONS @"Artful weapons"
+#define CHARACTER_ABILITY_BRUTALWEAPONS @"Brutal weapons"
+#define CHARACTER_ABILITY_PROJECTILEWEAPONS @"Projectile weapons"
+#define CHARACTER_ABILITY_CRITICALSTRIKES @"Critical strikes"
+#define CHARACTER_ABILITY_REND @"Rend"
+#define CHARACTER_ABILITY_DEFENSIVEPAUSE @"Defensive pause"
+#define CHARACTER_ABILITY_STRENGTHWITHIN @"Strength within"
+#define CHARACTER_ABILITY_MULTIPLEATTACKS @"Multiple attacks"
+#define CHARACTER_ABILITY_LEAPFLOAT @"Leap/float"
+#define CHARACTER_ABILITY_SPELLSWORD @"Spellsword"
+#define CHARACTER_ABILITY_DAOISM @"Daoism"
+#define CHARACTER_ABILITY_OFFWORLDCONTACT @"Offworld contact"
+#define CHARACTER_ABILITY_PRIMALFIRE @"Primal fire"
+#define CHARACTER_ABILITY_PRIMALAIR @"Primal air"
+#define CHARACTER_ABILITY_PRIMALWATER @"Primal water"
+#define CHARACTER_ABILITY_PRIMALEARTH @"Primal earth"
+#define CHARACTER_ABILITY_SHIFTINGBODY @"Shifting (body)"
+#define CHARACTER_ABILITY_SHIFTINGMINDSPIRIT @"Shifting (mind, spirit)"
+
+
+
+- (id)dataWithAllStats {
+    return [self copy];
+}
+
+- (id)dataWithStatGroup:(t_characterData)group;
+
++ (NSString *)headingFrom:(id)characterData;
+
++ (NSInteger)numberOfSectionsFrom:(id)characterData;
+
++ (NSString *)numberOfEntriesFrom:(id)characterData atSection:(NSInteger)section;
+
++ (NSString *)sectionHeadingFrom:(id)characterData atSection:(NSInteger)section;
+
++ (NSString *)itemDescriptionFrom:(id)characterData atIndexPath:(NSIndexPath *)indexPath;
+
++ (NSNumber *)itemValueFrom:(id)characterData atIndexPath:(NSIndexPath *)indexPath;
+
++ (t_characterDataElement)itemElementFrom:(id)characterData atIndexPath:(NSIndexPath *)indexPath;
+
+
+
 
 @implementation CharacterData
 @synthesize abilityStats = _abilityStats;
